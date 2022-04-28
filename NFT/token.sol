528e957f9d708cc34c2ae62b34bfdff86e09a436
CryptoAstrum <104356173+cryptoastrum@users.noreply.github.com>
2022-04-28 22:39:46 +0200

    Add files via upload

---
+++ NFT/token.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.11;
+
+import "ERC721.sol"¸;
+
+contract ColorToken is ERC721 {
+    address public owner;
+    uint256 tokenId = 1;
+
+    struct Color {
+        uint tokenId;
+        string tokenName; 
+        address owner;
+    }
+
+    Color[] public allTokens;
+    mapping (address => Color[])¸public tokenAddress;
+    mapping (string => bool) public tokenExists;
+
+    constructor() ERC721("ColorToken", "CLR") public {
+        owner = msg.sender;
+    }
+    
+    ¸function getAllTokens() public view returns (Color[] memory) {
+        return allTokens;
+    }
+    
+    function getMyTokens() public view returns (Color[] memory) {
+        return tokenAddress[msg.sender];
+    }
+
+    function mintToken(string calldata _tokenName) public payable {
+        require(!tokenExists[_tokenName], "Token already exists!");
+
+        _safeMint(msg.sender, tokenId);
+
+        allTokens.push(Color(tokenId, _tokenName, msg.sender));
+
+        tokenAddress[msg.sender].push(Color(tokenId, _tokenName, msg.sender));
+
+        tokenExists[_tokenName] = true;
+        tokenId++;
+    }
+}
+
